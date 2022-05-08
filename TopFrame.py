255f6ac190c116b9a7fce00df25bdd321d2d6dca
Leon_st2 <leonstiffel@gmail.com>
2022-05-08 16:24:19 +0200

    Import sql.connector for db access. Remote access isn't working. Not all permissions in PHPMyAdmin

--- TopFrame.py
+++ TopFrame.py
@@ -1,11 +1,11 @@
 from tkinter import *
 import colorpalet as cp
+import mysql.connector
 
 
 class TopFrame(Frame):
 
     def __init__(self, master, posx, posy, width, height, bgcolor):
-
         super().__init__(master, height=height, width=width)
 
         def hoverEnter(event, btn):
@@ -13,6 +13,17 @@ class TopFrame(Frame):
 
         def hoverLeave(event, btn):
             btn.config(bg=cp.lightGrey, fg=cp.closeRed)
+
+        def testDB():
+            db = mysql.connector.connect(
+                host="localhost",
+                user="1inf1tempsens",
+                password="awHqqS9SS0wtiqbW",
+                database="1inf1tempsens"
+            )
+            curser = db.cursor()
+
+            curser.execute("CREATE TABLE Test (test VARCHAR(50))")
 
         topPanel = Frame(master, width=width, height=height, bg=bgcolor)
         topPanel.place(x=posx, y=posy)
@@ -33,3 +44,14 @@ class TopFrame(Frame):
         titleText = Label(topPanel, text="Temperature Overview")
         titleText.place(relx=0.5, rely=0.5, anchor=CENTER)
         titleText.config(font=("Arial", 22, "underline", "bold"))
+
+        testBtn = Button(topPanel, width=5,
+                         bg=cp.lightGrey,
+                         fg=cp.closeRed,
+                         activebackground=cp.closeRed,
+                         text="TEST",
+                         bd=0,
+                         command=testDB,
+                         relief=SUNKEN)
+        testBtn.place(relx=0.2, rely=0.5, anchor=CENTER)
+        testBtn.config(font=("Arial", 12, "bold"))
